8a0f62d720aa9861fa9238936f1b469b032f3276
David Mallasen <dmallase@ucm.es>
2020-02-20 13:14:56 +0100

    Redo graphics

--- graphics.m
+++ graphics.m
@@ -1,29 +1,26 @@
-if size(x, 2) == 1
-    plot(t, x)
-elseif size(x, 2) == 2
-    figure(1)
-    subplot(2, 1, 1)
-    plot(t, x(:, 1))
-    subplot(2, 1, 2)
-    plot(t, x(:, 2))
-    
-    pause(2)
-    
-    figure(2)
-    plot(x(:, 1), x(:, 2))
-elseif size(x, 2) == 3
-    figure(1)
-    subplot(3, 1, 1)
-    plot(t, x(:, 1))
-    subplot(3, 1, 2)
-    plot(t, x(:, 2))
-    subplot(3, 1, 3)
-    plot(t, x(:, 3))
-    
-    pause(2)
-    
-    figure(2)
-    plot3(x(:, 1), x(:, 2), x(:, 3))
-else
+dim = size(x, 2);
+colors = ['r', 'g', 'b'];
+
+if dim > 3 || dim < 1
     fprintf("x dimension not supported: %i\n", size(x, 2))
+end
+
+% Plot each coord separately
+figure(1)
+for i = 1:dim
+    subplot(dim, 1, i)
+    plot(t, x(:, i), colors(i))
+    title(sprintf("Coord %d of the solution", i))
+end
+
+% Plot the trajectory of the solution
+if dim > 1
+    pause(2)
+    figure(2)
+    if dim == 2
+        plot(x(:, 1), x(:, 2), 'r')
+    else  % dim == 3
+        plot(x(:, 1), x(:, 2), x(:, 3), 'r')
+    end
+    title("Trajectory of the solution")
 end
